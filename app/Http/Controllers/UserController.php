4df98f9304ff1f5b567b08581ea9afdcf60b99bf
kaushal khunt <32392734+kmkhunt@users.noreply.github.com>
2020-05-12 11:13:30 +0530

    just test2
    just test2 description

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function index(Request $request) {
-        echo "this is just test and resolve line and resolve line test again";
+        echo "this is just test2";
 		return view("user.index");
 	}
 
